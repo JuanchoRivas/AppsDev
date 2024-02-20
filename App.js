647cb24038bd7a72457a91987844cd3658dc3e57
Juan Marcos Rivas <101603333+JuanchoRivas@users.noreply.github.com>
2024-02-20 18:25:23 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import { StyleSheet } from 'react-native';
-
-import Home from './src/components/Screens/Home';
+import { useFonts } from "expo-font";
+import { useState } from "react";
+import Home from "./src/components/Screens/Home";
 import ItemListCategories from './src/components/Screens/ItemListCategories';
+import { fonts } from './src/components/Global/fonts';
 
 export default function App() {
+  const [ categorySelected, setCategorySelected ] = useState('');
+
+  const [fontsLoaded] = useFonts(fonts);
+
+  if(!fontsLoaded) {
+    return null;
+  }
 
   return (
     <>
-    {/*<Home />*/}
-    <ItemListCategories category="lighting"/>
+  {categorySelected ? (
+    <ItemListCategories />
+  ) : (
+    <Home setCategorySelected={setCategorySelected}/>
+  )}
     </>
-
+  
   );
 }
 
